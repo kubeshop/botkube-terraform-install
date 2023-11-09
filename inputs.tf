797be427608843258ec16d69f3047f60c1279036
Maria Ashby <100232008+mashby2022@users.noreply.github.com>
2023-11-09 13:20:49 -0500

    Create inputs.tf

---
+++ inputs.tf
@@ -0,0 +1,11 @@
+variable "BOTKUBE_MS_TEAMS_APPLICATION_ID" {
+    type = string
+}
+
+variable "BOTKUBE_MS_TEAMS_APPLICATION_PASSWORD" {
+  type = string
+}
+
+variable "BOTKUBE_MS_TEAMS_BOT_NAME" {
+  type = string
+}
